2bf97e626c49b3ff607c6ba01e4bb935856df528
marilemos <43227799+marilemos@users.noreply.github.com>
2025-06-29 22:43:52 +0000

    resolucao2.4

--- exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
+++ exercicios/2-construindo-consultas/4-transformando-dados-textuais.sql
@@ -1,16 +1,50 @@
 -- Crie uma consulta que retorne as colunas CustomerId, FirstName, LastName e Address das pessoas consumidoras brasileiras
-
-
+SELECT 
+CustomerId, 
+FirstName, 
+LastName, 
+Address
+FROM customers
+WHERE Country LIKE 'Bra%';
 -- Na consulta anterior, converta o sobrenome para letras maiúsculas
-
+SELECT 
+CustomerId, 
+FirstName, 
+UPPER (LastName), 
+Address
+FROM customers
+WHERE Country LIKE 'Bra%';
 
 -- Na consulta anterior, converta o nome para letras minúsculas
+SELECT 
+CustomerId, 
+LOWER (FirstName) AS nome, 
+UPPER (LastName) AS  sobrenome, 
+Address AS endereço
+FROM customers
+WHERE Country LIKE 'Bra%';
 
 
 -- Na consulta anterior, adicione uma coluna cujos os dados seja o resultado da concatenação das colunas de nome e sobrenome
+SELECT 
+CustomerId, 
+LOWER (FirstName) AS nome, 
+UPPER (LastName) AS  sobrenome, 
+FirstName || ' ' || LastName as nome_completo,
+Address AS endereço
+FROM customers
+WHERE Country LIKE 'Bra%';
 
 
 -- Na consulta anterior, substitua "Av." por "Avenida" na coluna endereço
+SELECT 
+CustomerId, 
+LOWER (FirstName) AS nome, 
+UPPER (LastName) AS  sobrenome, 
+FirstName || ' ' || LastName as nome_completo,
+replace (Address, 'Av.', 'Avenida') AS endereço
+FROM customers
+WHERE Country LIKE 'Bra%';
 
 
 -- Na consulta anterior, utilize o símbolo de coringa para construir padrões textuais
